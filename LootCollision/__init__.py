e02052e0c6310fc99fc7ff912dd75f53aa6a888a
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2024-10-08 16:35:55 -0400

    Add files via upload

---
+++ LootCollision/__init__.py
@@ -0,0 +1,28 @@
+from unrealsdk import RegisterHook, RemoveHook, UObject, UFunction, FStruct,Log #type: ignore
+from Mods.ModMenu import EnabledSaveType, RegisterMod, SDKMod
+
+
+def EnableRagdoll(caller: UObject, function: UFunction, params: FStruct):
+    if not caller.CollisionComponent:
+        return True
+        
+    caller.CollisionComponent.SetRBCollidesWithChannel(16, False)#Dead pawns
+    caller.CollisionComponent.SetRBCollidesWithChannel(24, False)#willow pickups
+    return False
+
+
+class LootCollision(SDKMod):
+    Name = "Loot Collision"
+    Description = f"Choose if loot collides with dead bodies or other loot."
+    Author = "RedxYeti"
+    Version = "1.0"
+    SaveEnabledState = EnabledSaveType.LoadWithSettings
+
+    def Enable(self) -> None:
+        RegisterHook("WillowGame.WillowPickup.EnableRagdollCollision", "EnableRagdoll", EnableRagdoll)
+
+    def Disable(self) -> None:
+        RemoveHook("WillowGame.WillowPickup.EnableRagdollCollision", "EnableRagdoll")
+
+LC = LootCollision()
+RegisterMod(LootCollision())
